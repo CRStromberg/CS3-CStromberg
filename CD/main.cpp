6b95fa654c10f3a2adea87e00bbeeeaa60324a24
CRStromberg <cstromberg86@gmail.com>
2020-02-20 17:29:02 -0700

    .

--- CD/main.cpp
+++ CD/main.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 void test(vector <int unsigned> &Jack, vector <int unsigned> &Jill);
 void read(vector <int unsigned> &Jack, vector <int unsigned> &Jill);
-int ans(vector <int unsigned> &Jack, vector <int unsigned> &Jill, int jkN, int jlN);
+int ans(vector <int unsigned> &Jack, vector <int unsigned> &Jill, int jkN);
 
 int main(int argc, char* argv[])
 {
@@ -42,52 +42,37 @@ int main(int argc, char* argv[])
 
 void test(vector <int unsigned> &Jack, vector <int unsigned> &Jill)
 {
-    int jkn, jln;
+    int jkn;
 
     //Test case 1
     jkn = 12;
-    jln = 10;
     Jack = {4, 5, 7, 9, 10, 13, 23, 27, 28, 29, 31, 33};
     Jill = {1, 3, 6, 7, 13, 15, 25, 28, 32, 33};
-    assert(ans(Jack, Jill, jkn, jln) == 4);
+    assert(ans(Jack, Jill, jkn) == 4);
     
     //Test case 2
     jkn = 23;
-    jln = 27;
     Jack = {1, 2, 3, 5, 6, 8, 9, 10, 11, 13, 14, 15, 19, 23, 27, 28, 29, 31, 33, 40, 41, 44, 49};
     Jill = {1, 2, 3, 4, 6, 7, 9, 11, 13, 15, 19, 21, 25, 28, 32, 33, 35, 36, 37, 41, 43, 44, 48, 52, 55, 60, 63};
-    assert(ans(Jack, Jill, jkn, jln) == 13);
+    assert(ans(Jack, Jill, jkn) == 13);
     
     //Test case 3
     jkn = 10;
-    jln = 11;
     Jack = {1, 2, 10, 11, 23, 27, 28, 51, 54, 79};
     Jill = {1, 4, 19, 21, 25, 28, 37, 54, 55, 60, 63};
-    assert(ans(Jack, Jill, jkn, jln) == 3);
+    assert(ans(Jack, Jill, jkn) == 3);
     
     cout<<"All Test Cases Passed...\n";
 }
 
-int ans(vector <int unsigned> &Jack, vector <int unsigned> &Jill, int jkN, int jlN)
+int ans(vector <int unsigned> &Jack, vector <int unsigned> &Jill, int jkN)
 {
-    int i;
     int answer = 0;
-
-    //Determine shortest vector and search the largest (fewer loops) using binary_search()
-    if(Jack.size() > Jill.size())
+        
+    for( int i =0; i<jkN; i++)
     {
-        for(i=0; i<jlN; i++)
-        {
-            if (binary_search(Jack.begin(), Jack.end(), Jill.at(i))) { answer++;}
-        }
-    }
-    else
-    {
-        for(i =0; i<jkN; i++)
-        {
-            if (binary_search(Jill.begin(), Jill.end(), Jack.at(i))) { answer++;}
-        }        
-    }
+        if (binary_search(Jill.begin(), Jill.end(), Jack.at(i))) { answer++;}
+    }        
     return answer;
 }
 
@@ -108,5 +93,5 @@ void read(vector <int unsigned> &Jack, vector <int unsigned> &Jill)
         cin >> temp;
         Jill.push_back(temp);
     }
-    cout << ans(Jack, Jill, jkn, jln)<<endl;
+    cout << ans(Jack, Jill, jkn)<<endl;
 }

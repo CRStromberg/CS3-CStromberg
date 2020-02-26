184f2b53e054eafa766149b0742dccd90cca01fa
CRStromberg <cstromberg86@gmail.com>
2020-02-25 18:40:01 -0700

    Putovanje Algorithm Done

---
+++ Putovanje/putovanje.cpp
@@ -0,0 +1,15 @@
+/*
+Kattis Problem
+Putovanje
+
+    Algorithm
+-----------------
+-   Read Data
+        - first line - N is array of fruits, C is Capacity of Mislav stomach
+        - Second Line - weight of fruits in order of discovery
+-   Write test cases
+-   Compute data
+    -   use nested loop to loop though vector getting max fruit eaten from each starting point
+    -   Set total number of fruits to the largest max fruit eaten
+-   Output the total number of fruits
+*/
